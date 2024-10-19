2482530bafb71e6d91127ded92d16936736acc37
Alaa Emad <alaaemadalhamzi@gmail.com>
2024-10-19 10:24:56 +0300

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Alaa Emad Alhamzi on 16/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
